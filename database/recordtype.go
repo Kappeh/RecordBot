4c90293665167683590ef371260e5caed5f4af90
Kappeh <kieranleep@hotmail.com>
2020-02-05 00:28:47 +0000

    finished link functions in database package

--- database/recordtype.go
+++ database/recordtype.go
@@ -1,18 +1,167 @@
 package database
 
+import (
+	"strconv"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
 // GuildRecordTypeChannel gets the guild record type channel for the record type for
 // a specified guild
-func (r RecordType) GuildRecordTypeChannel(guildID string) (GuildRecordTypeChannel, error) {
-	return GuildRecordTypeChannel{}, nil
+func (rt RecordType) GuildRecordTypeChannel(guildID string) (GuildRecordTypeChannel, bool, error) {
+	db, err := Instance()
+	if err != nil {
+		return GuildRecordTypeChannel{}, false, errors.Wrap(err, "couldn't get database instance")
+	}
+	grtc, ok, err := db.GuildRecordTypeChannel(guildID, rt.ID)
+	if err != nil {
+		return GuildRecordTypeChannel{}, false, errors.Wrap(err, "failed to determine if guild record type channel exists")
+	}
+	return grtc, ok, nil
 }
 
 // GuildRecordTypeChannels gets the guild record type channels for the record type
 // for all guilds
-func (r RecordType) GuildRecordTypeChannels() ([]GuildRecordTypeChannel, error) {
-	return nil, nil
+func (rt RecordType) GuildRecordTypeChannels() ([]GuildRecordTypeChannel, error) {
+	db, err := Instance()
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't get database instance")
+	}
+	// Convert id to int
+	recordTypeIDint, err := strconv.Atoi(rt.ID)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to convert id to integer")
+	}
+	// Query the database
+	rows, err := db.db.Query(`
+		SELECT GuildID, ChannelID, Timestamp, EditedTimestamp
+		FROM GuildRecordTypeChannels
+		WHERE RecordTypeID = ?
+	`, recordTypeIDint)
+	if err != nil {
+		return nil, errors.Wrap(err, "database query failed")
+	}
+	defer rows.Close()
+	// Create space to store results
+	results := []GuildRecordTypeChannel{}
+	var (
+		guildIDint            int
+		channelIDint          int
+		timestampString       string
+		editedTimestampString string
+		timestamp             time.Time
+		editedTimestamp       time.Time
+	)
+	// For each row
+	for rows.Next() {
+		// Extract data
+		if err = rows.Scan(
+			&guildIDint, &channelIDint,
+			&timestampString, &editedTimestampString,
+		); err != nil {
+			return nil, errors.Wrap(err, "failed to extract data")
+		}
+		// Parse timestamps
+		if timestamp, err = time.Parse(timeLayout, timestampString); err != nil {
+			return nil, errors.Wrap(err, "failed to parse timestamp")
+		}
+		if editedTimestamp, err = time.Parse(timeLayout, editedTimestampString); err != nil {
+			return nil, errors.Wrap(err, "failed to parse edited timestamp")
+		}
+		// Add to results
+		results = append(results, GuildRecordTypeChannel{
+			GuildID:         strconv.Itoa(guildIDint),
+			RecordTypeID:    rt.ID,
+			ChannelID:       strconv.Itoa(channelIDint),
+			Timestamp:       Timestamp(timestamp),
+			EditedTimestamp: Timestamp(editedTimestamp),
+		})
+	}
+	return results, nil
 }
 
 // Records get the all the records that fall into the record type
-func (r RecordType) Records() ([]Record, error) {
-	return nil, nil
+func (rt RecordType) Records() ([]Record, error) {
+	db, err := Instance()
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't get database instance")
+	}
+	// Convert id to int
+	recordTypeIDint, err := strconv.Atoi(rt.ID)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to convert id to integer")
+	}
+	// Query the database
+	rows, err := db.db.Query(`
+		SELECT ID, Verified, VerifierID, VerifiedTimestamp, UpdateRequest,
+			UpdateRequestRecordID, EditionID, BuildClassID, Name, Description,
+			SubmitterID, Timestamp, EditedTimestamp
+		FROM Records
+		WHERE RecordTypeID = ?
+	`, recordTypeIDint)
+	if err != nil {
+		return nil, errors.Wrap(err, "database query failed")
+	}
+	defer rows.Close()
+	// Create space to store results
+	results := []Record{}
+	var (
+		idInt                    int
+		verifiedInt              int
+		verifierIDint            int
+		verifiedTimestampString  string
+		updateRequestInt         int
+		updateRequestRecordIDint int
+		editionIDint             int
+		buildClassIDint          int
+		name                     string
+		description              string
+		submitterIDint           int
+		timestampString          string
+		editedTimestampString    string
+		verifiedTimestamp        time.Time
+		timestamp                time.Time
+		editedTimestamp          time.Time
+	)
+	// For each row
+	for rows.Next() {
+		// Extract data
+		if err = rows.Scan(
+			&idInt, &verifiedInt, &verifierIDint, &verifiedTimestampString,
+			&updateRequestInt, &updateRequestRecordIDint, &editionIDint,
+			&buildClassIDint, &name, &description, &submitterIDint,
+			&timestampString, &editedTimestampString,
+		); err != nil {
+			return nil, errors.Wrap(err, "failed to extract data")
+		}
+		// Parse timestamps
+		if verifiedTimestamp, err = time.Parse(timeLayout, verifiedTimestampString); err != nil {
+			return nil, errors.Wrap(err, "failed to parse verified timestamp")
+		}
+		if timestamp, err = time.Parse(timeLayout, timestampString); err != nil {
+			return nil, errors.Wrap(err, "failed to parse timestamp")
+		}
+		if editedTimestamp, err = time.Parse(timeLayout, editedTimestampString); err != nil {
+			return nil, errors.Wrap(err, "failed to parse edited timestamp")
+		}
+		// Add to results
+		results = append(results, Record{
+			ID:                    strconv.Itoa(idInt),
+			Verified:              verifiedInt != 0,
+			VerifierID:            strconv.Itoa(verifierIDint),
+			VerifiedTimestamp:     Timestamp(verifiedTimestamp),
+			UpdateRequest:         updateRequestInt != 0,
+			UpdateRequestRecordID: strconv.Itoa(updateRequestRecordIDint),
+			EditionID:             strconv.Itoa(editionIDint),
+			BuildClassID:          strconv.Itoa(buildClassIDint),
+			RecordTypeID:          strconv.Itoa(recordTypeIDint),
+			Name:                  name,
+			Description:           description,
+			SubmitterID:           strconv.Itoa(submitterIDint),
+			Timestamp:             Timestamp(timestamp),
+			EditedTimestamp:       Timestamp(editedTimestamp),
+		})
+	}
+	return results, nil
 }
--- database/version.go
+++ database/version.go
@@ -1,16 +1,95 @@
 package database
 
+import (
+	"strconv"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
 // Edition gets the edition of the version
-func (v Version) Edition() (Edition, error) {
-	return Edition{}, nil
+func (v Version) Edition() (Edition, bool, error) {
+	db, err := Instance()
+	if err != nil {
+		return Edition{}, false, errors.Wrap(err, "couldn't get database instance")
+	}
+	e, ok, err := db.Edition(v.EditionID)
+	if err != nil {
+		return Edition{}, false, errors.Wrap(err, "failed to determine if edition exists")
+	}
+	return e, ok, nil
 }
 
 // BuildVersion gets the build version of the version for a specified build
-func (v Version) BuildVersion(buildID string) (BuildVersion, error) {
-	return BuildVersion{}, nil
+func (v Version) BuildVersion(buildID string) (BuildVersion, bool, error) {
+	db, err := Instance()
+	if err != nil {
+		return BuildVersion{}, false, errors.Wrap(err, "couldn't get database instance")
+	}
+	bv, ok, err := db.BuildVersion(buildID, v.ID)
+	if err != nil {
+		return BuildVersion{}, false, errors.Wrap(err, "failed to determine if build version exists")
+	}
+	return bv, ok, nil
 }
 
 // BuildVersions gets the build versions of the version for all builds
 func (v Version) BuildVersions() ([]BuildVersion, error) {
-	return nil, nil
+	db, err := Instance()
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't get database instance")
+	}
+	// Convert id to int
+	versionIDint, err := strconv.Atoi(v.ID)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to convert version id to integer")
+	}
+	// Query the database
+	rows, err := db.db.Query(`
+		SELECT BuildID, StatusID, Notes, Timestamp, EditedTimestamp
+		FROM BuildVersions
+		WHERE VersionID = ?
+	`, versionIDint)
+	if err != nil {
+		return nil, errors.Wrap(err, "database query failed")
+	}
+	defer rows.Close()
+	// Create space to store results
+	results := []BuildVersion{}
+	var (
+		buildIDint            int
+		statusIDint           int
+		notes                 string
+		timestampString       string
+		editedTimestampString string
+		timestamp             time.Time
+		editedTimestamp       time.Time
+	)
+	// For each row
+	for rows.Next() {
+		// Extract data
+		if err = rows.Scan(
+			&buildIDint, &statusIDint, &notes,
+			&timestampString, &editedTimestampString,
+		); err != nil {
+			return nil, errors.Wrap(err, "failed to extract data")
+		}
+		// Parse timestamps
+		if timestamp, err = time.Parse(timeLayout, timestampString); err != nil {
+			return nil, errors.Wrap(err, "failed to parse timestamp")
+		}
+		if editedTimestamp, err = time.Parse(timeLayout, editedTimestampString); err != nil {
+			return nil, errors.Wrap(err, "failed to parse edited timestamp")
+		}
+		// Add to results
+		results = append(results, BuildVersion{
+			BuildID:         strconv.Itoa(buildIDint),
+			VersionID:       v.ID,
+			StatusID:        strconv.Itoa(statusIDint),
+			Notes:           notes,
+			Timestamp:       Timestamp(timestamp),
+			EditedTimestamp: Timestamp(editedTimestamp),
+		})
+	}
+	return results, nil
 }
